987076128306aa715a8d6c9117542e5b590df929
Veguitaa <webav2020@gmail.com>
2022-01-20 23:14:08 -0300

    crud de producto 1 y 2

---
+++ src/controllers/mainController.js
@@ -0,0 +1,32 @@
+const fs = require('fs');
+const path = require('path');
+
+const productsFilePath = path.join(__dirname, '../data/productsDataBase.json');
+let products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+
+
+const toThousand = n => n.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+
+const controller = {
+	index: (req, res) => {
+		products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
+		const offer = products.filter(element => element.category === "in-sale")
+		const visited = products.filter(element => element.category === "visited")
+		res.render('index', {
+			offer,
+			visited,
+			toThousand,
+		})
+	},
+	search: (req, res) => {
+		const search = req.query.keywords.trim()
+		if (search !== '') {
+			const result = products.filter(product => product.name.toLowerCase().includes(search.toLowerCase()))
+			res.render('results', {result, toThousand, search})
+		} else {
+			res.redirect('/')
+		}
+	},
+};
+
+module.exports = controller;
---
+++ src/routes/products.js
@@ -0,0 +1,41 @@
+// ************ Require's ************
+const express = require('express');
+const router = express.Router();
+const multer = require('multer');
+const path = require('path');
+
+// ************ Controller Require ************
+const productsController = require('../controllers/productsController');
+
+const storage = multer.diskStorage({
+    destination: function (req, file, callback) {
+      callback(null, './public/images/products')
+    },
+    filename: function (req, file, callback) {
+      callback(null, `img-${Date.now()}${path.extname(file.originalname)}`)
+    }
+  })
+  
+const upload = multer({ storage: storage })
+
+/*** GET ALL PRODUCTS ***/ 
+router.get('/', productsController.index); 
+
+/*** CREATE ONE PRODUCT ***/ 
+router.get('/create/', productsController.create); 
+router.post('/', upload.single('image'), productsController.store); 
+
+
+/*** GET ONE PRODUCT ***/ 
+router.get('/detail/:id/', productsController.detail); 
+
+/*** EDIT ONE PRODUCT ***/ 
+router.get('/edit/:id', productsController.edit); 
+router.put('/edit/:id', productsController.update);
+
+
+/*** DELETE ONE PRODUCT***/ 
+router.delete('/delete/:id', productsController.destroy);
+
+
+module.exports = router;
